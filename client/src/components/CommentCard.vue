96558288976b0b940eb09fd3446a51f5f3236465
Your Name <Your email>
2019-07-20 01:05:09 -0600

    Icon Resized

--- client/src/components/CommentCard.vue
+++ client/src/components/CommentCard.vue
@@ -1,45 +1,44 @@
 <template>
   <div class="CommentCard">
-    <b-row class="my-1">
-      <b-col cols="1"></b-col>
-      <b-col cols="11">
-        <b-card header-tag="header" footer-tag="footer">
-          <h6 slot="header" class="mb-0">{{ comment.author }}</h6>
-          <b-card-text>{{ comment.text }}</b-card-text>
-          <div slot="footer" class="mb-0">
-            <CommentVote :pID="pID"></CommentVote>
-          </div>
-        </b-card>
-      </b-col>
+    <b-row>
+      <b-card header-tag="header" footer-tag="footer">
+        <h6 slot="header" class="mb-0">{{ comment.author }}</h6>
+        <b-card-text>{{ comment.text }}</b-card-text>
+        <h6 slot="footer" class="mb-0">Score: {{ comment.score }}</h6>
+      </b-card>
     </b-row>
   </div>
 </template>
 
 
 <script>
-  import CommentVote from "@/components/CommentVote";
+import CommentVote from "@/components/CommentVote";
 
-  export default {
-    name: 'CommentCard',
-    data() {
-      return {}
-    },
-    props: [
-      "pID"
-    ],
-    computed: {
-      comment() {
-        return this.$store.state.comments.find(el => el._id == this.pID)
-      }
-    },
-    methods: {},
-    components: {
-      CommentVote
+export default {
+  name: "CommentCard",
+  data() {
+    return {};
+  },
+  props: ["pID"],
+  computed: {
+    comment() {
+      return this.$store.state.comments.find(el => el._id == this.pID);
     }
+  },
+  methods: {
+    switchView() {
+      this.$router.push({
+        name: "CommentDisplay",
+        params: { commentID: this.cID }
+      });
+    }
+  },
+  components: {
+    CommentVote
   }
+};
 </script>
 
 
 <style scoped>
-
 </style>
