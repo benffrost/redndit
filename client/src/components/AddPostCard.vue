c3edec8281e1b62eddfce309a8711f3a9949fe4e
ChristopherOBerry <chroberry@gmail.com>
2019-07-19 23:32:24 -0600

    posts submitting and displaying

--- client/src/components/AddPostCard.vue
+++ client/src/components/AddPostCard.vue
@@ -3,16 +3,38 @@
     <form class="col-8" @submit.prevent="addPost">
       <div class="form-group">
         <label for="title">Title:</label>
-        <input type="text" name="title" class="form-control" id="title" placeholder="Post title" />
+        <input
+          type="text"
+          name="title"
+          class="form-control"
+          id="title"
+          placeholder="Post title"
+          v-model="post.title"
+        />
       </div>
       <div class="form-group">
         <label for="name">Enter your name:</label>
-        <input type="text" name="name" class="form-control" id="name" placeholder="username" />
+        <input
+          type="text"
+          name="name"
+          class="form-control"
+          id="name"
+          placeholder="username"
+          v-model="post.author"
+        />
       </div>
       <div class="form-group">
         <label for="body">Your message:</label>
-        <input type="text" name="body" class="form-control" id="body" placeholder="Post body" />
+        <input
+          type="text"
+          name="body"
+          class="form-control"
+          id="body"
+          placeholder="Post body"
+          v-model="post.text"
+        />
       </div>
+      <b-button type="submit" variant="outline-primary">Submit Post</b-button>
     </form>
   </div>
 </template>
@@ -22,10 +44,20 @@
 export default {
   name: "addPost",
   data() {
-    return {};
+    return {
+      post: {
+        title: "",
+        author: "",
+        text: ""
+      }
+    };
   },
   computed: {},
-  methods: { addPost() {} },
+  methods: {
+    addPost() {
+      this.$store.dispatch("addPost", this.post);
+    }
+  },
   components: {}
 };
 </script>
