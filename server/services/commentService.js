f362053489170d90ac8ba67d014f1f841af5be1b
benffrost <benffrost@gmail.com>
2019-07-20 01:00:54 -0600

    more requiredments

--- server/services/commentService.js
+++ server/services/commentService.js
@@ -7,7 +7,7 @@ let _schema = new mongoose.Schema({
   author: { type: String, maxlength: 60, required: true },
   text: { type: String, required: true },
   score: { type: Number, required: true },
-  post: [{ type: ObjectId, ref: 'Post' }, required: true]
+  post: [{ type: ObjectId, ref: 'Post', required: true }]
 }, { timestamps: true })
 
 export default mongoose.model('Comment', _schema)
