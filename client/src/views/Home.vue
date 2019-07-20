471006e16555e300c09c6bffc2bcc291ae0f4794
benffrost <benffrost@gmail.com>
2019-07-20 02:09:17 -0600

    sort by date

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <div>Sort By:
-      <b-btn @click="sortByDate">Date</b-btn>
+      <b-btn @click="sortByDate">Recent</b-btn>
       <b-btn @click="sortByScore">Score</b-btn>
     </div>
     <PostCard v-for="post in posts" :key="post._id" :pID="post._id"></PostCard>
@@ -31,22 +31,10 @@
     },
     methods: {
       sortByDate() {
-        //WIP for now just refresh from server?
-        // function convertDatetoNumber(date) {
-
-        //   let years = parseFloat(date.slice(0,4))-2019
-        //   let months = years*12 + parseFloat(date.slice(5, 7))
-        //   let days = months*30 + parseFloat(date)          let seconds = parseFloat(date.slice(17, 24))
-
-        //   return seconds
-        // }
-        // let x = "2019-07-20T05:20:33.498Z"
-        // let y = convertDatetoNumber(x)
-        // debugger
-        // this.$store.dispatch("sortPosts", function (a, b) { return convertDatetoNumber(a.createdAt) > convertDatetoNumber(b.createdAt) })
+        this.$store.dispatch("sortPosts", function (b, a) { return Date.parse(a.createdAt) - Date.parse(b.createdAt) })
       },
       sortByScore() {
-        this.$store.dispatch("sortPosts", function (a, b) { return a.score - b.score })
+        this.$store.dispatch("sortPosts", function (b, a) { return a.score - b.score })
       }
     }
 
